b45ed59c3295a95855eb5afc7df33fd868bd886a
AliouSang <aliouferg@yahoo.com>
2026-08-16 20:02:24 +0000

    add operate functionality

--- script.js
+++ script.js
@@ -5,3 +5,19 @@ const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 
 const divide = (a, b) => a / b;
+
+let num1, op, num2;
+
+const operate = (op, num1, num2) => {
+  let result;
+  if (op === "+") {
+    result = add(num1, num2)  
+  } else if(op === "-") {
+    result = subtract(num1, num2);
+  } else if (op === "*") {
+    result = multiply(num1, num2);
+  } else if (op === "/") {
+    result = divide(num1, num2);
+  }
+  return result;
+}
